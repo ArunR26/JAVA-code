5d3ef1f833b4ff11df5836d735689257611c4aab
ArunR26 <163302607+ArunR26@users.noreply.github.com>
2024-09-03 10:22:07 +0530

    Add files via upload

---
+++ oddoreven.java
@@ -0,0 +1,19 @@
+package dsa;
+
+import java.util.Scanner;
+
+public class oddoreven {
+	    public static void main(String[] args) {
+	        Scanner scanner = new Scanner(System.in);
+	        System.out.print("Enter an integer: ");
+	        int number = scanner.nextInt();
+	      
+	        if (number % 2 == 0) {
+	            System.out.println(number + " is even number5.");
+	        } else {
+	            System.out.println(number + " is odd number.");
+	        }
+	        scanner.close();
+	    }
+	}
+
