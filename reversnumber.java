83ecce3c17ade7de102e0b98e3ecab6477709f0f
ArunR26 <163302607+ArunR26@users.noreply.github.com>
2024-09-03 10:30:18 +0530

    Update reversenumber.java

--- reversnumber.java
+++ reversnumber.java
@@ -1,20 +1,20 @@
 package dsa;
 import java.util.Scanner;
 
-public class reversnumber {
+public class reversenumber {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
         System.out.print("Enter a number: ");
         int number = scanner.nextInt();
         
-        int reversnumber = 0;
+        int reversenumber = 0;
         
         while (number != 0) {
             int digit = number % 10;
-            reversnumber = reversnumber * 10 + digit;
+            reversenumber = reversenumber * 10 + digit;
             number /= 10;
         }
-        System.out.println("Reversed Number: " + reversnumber);
+        System.out.println("Reversed Number: " + reversenumber);
     }
 }
